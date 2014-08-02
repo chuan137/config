2aaa3be863a1c4a8aab0a1c13e4fb2da40aafb62
chuan miao <chuan137@gmail.com>
2014-08-02 20:28:52 +0200

    gether script into bin/

---
+++ bin/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+########################################################
+# setup.sh
+# @CM
+# set up symbolic links for dotfiles
+########################################################
+
+dir=~/etc/dotfiles
+olddir=~/etc/dotfiles_old
+files="mybashrc vimrc vim/pluginlist"
+
+if [[ ! -d ~/.vim ]]; then
+    mkdir ~/.vim
+    mkdir ~/.vim/bundle
+    cd ~/.vim/bundle
+    git clone https://github.com/gmarik/Vundle.vim
+    cd $dir
+fi
+
+# backup old files
+echo "> Back up old dotfiles ..."
+mkdir -p $olddir
+for f in $files; do
+    mv ~/.$f $olddir/
+done
+echo "done"
+
+
+# create symbolic links
+echo -n "> Create symbolic links for ..."
+for f in $files; do
+    echo -n "$f "
+    ln -s $dir/$f ~/.$f
+done
+echo "\ndone"
